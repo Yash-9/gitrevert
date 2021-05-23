ce0d3a2716266dee34a67f5bd5d8435001ccaa45
Yash-9 <yashbhawre11@gmail.com>
2021-05-23 17:28:12 +0000

    m4

--- p1.java
+++ p1.java
@@ -1,8 +1,11 @@
 public class p1 { 
     public static void main(strgs[]){
         System.out.print("line1");
+<<<<<<< HEAD
         System.out.print("line2");
         System.out.print("line3");
+=======
+>>>>>>> parent of 3018041... m2
        
     }
 
