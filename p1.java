ca31b676c69035551528d8fed8043357ff125013
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-10 19:31:49 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,12 +1,8 @@
 public class p1 { 
     public static void main(strgs[]){
         System.out.print("line1");
-<<<<<<< HEAD
         System.out.print("line2");
-        System.out.print("line3");
-=======
->>>>>>> parent of 3018041... m2
-       
+        System.out.print("line3");       
     }
 
 }
