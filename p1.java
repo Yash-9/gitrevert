49693511d8ad1050742a599ada2c7a94b33ad3ab
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-15 22:47:32 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -2,7 +2,10 @@ public class p1 {
     public static void findEvenOdd(int num)  
     {  
      //method body  
-     
+     if(num%2==0)   
+     System.out.println(num+" is even");   
+     else   
+     System.out.println(num+" is odd");  
     }  
     
     public static void main(strgs[]){
