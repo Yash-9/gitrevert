1765034b902d273988a6230e7715762d2db5b14e
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-15 22:49:26 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -11,6 +11,7 @@ public class p1 {
     }  
     
     public static void main(strgs[]){
+        Scanner scan=new Scanner(System.in);  
         System.out.print("line1");
         System.out.print("line2");
         System.out.print("line3");       
