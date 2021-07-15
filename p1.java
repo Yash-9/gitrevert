477a52ba62ceebf4e7bf4e8994260ec42328a16a
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-15 22:47:19 +0530

    added one method in p1.java

--- p1.java
+++ p1.java
@@ -1,4 +1,10 @@
 public class p1 { 
+    public static void findEvenOdd(int num)  
+    {  
+     //method body  
+     
+    }  
+    
     public static void main(strgs[]){
         System.out.print("line1");
         System.out.print("line2");
