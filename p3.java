a538add00502f717b50fde81e6036276110e9262
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-18 19:08:32 +0530

    Update p3.java

--- p3.java
+++ p3.java
@@ -1,9 +1,11 @@
 public class p3{
-    public static {
+    public static void callme(){
+        System.out.println("Wel-come from call me!!!");
     }
     public static void main (){
         System.out.println("Hi Yash");
         System.out.println("Welcome!!!");
-        System.out.println("Wel-come!!!");
+        
+        callme();
     }
 }
