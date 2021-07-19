8fdd06f4024d8d609148a48e604d5fcd17c5e239
Yash <80215154+Yash-9@users.noreply.github.com>
2021-07-19 11:44:39 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -13,8 +13,7 @@ public class p1 {
     public static void main(strgs[]){
         Scanner scan=new Scanner(System.in);  
         System.out.print("Enter number:-");
-        System.out.print("line2");
-        System.out.print("line3");       
+             
     }
 
 }
